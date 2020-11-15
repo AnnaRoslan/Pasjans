15e8c00c8d7c7504726788641e121a9b4f9f998f
Cezary Michalski <cezary.michalski2.stud@pw.edu.pl>
2020-11-15 22:33:55 +0100

    Moving from and to non existing stock tests.

---
+++ Pasjans/NUnitTest/CardsMovingTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Pasjans;
+using Pasjans.PlayingCard;
+
+namespace NUnitTest
+{
+    public class CardsMovingTest
+    {
+        private Table _table;
+        private CardMover _cardMover;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _table = new Table();
+            _cardMover = new CardMover();
+            MockTable();
+        }
+
+        private void MockTable()
+        {
+            _table.Stock1.Add(new Card(CardValue.Two, Color.Heart) {IsReversed = true});
+            _table.Stock2.Add(new Card(CardValue.Three, Color.Spade) {IsReversed = true});
+            _table.Stock3.Add(new Card(CardValue.Four, Color.Club) {IsReversed = true});
+            _table.Stock4.Add(new Card(CardValue.Two, Color.Diamond) {IsReversed = false});
+            _table.Stock5.Add(new Card(CardValue.Five, Color.Diamond) {IsReversed = true});
+            _table.Stock5.Add(new Card(CardValue.Four, Color.Spade) {IsReversed = true});
+            _table.Stock6.Add(new Card(CardValue.Three, Color.Diamond) {IsReversed = true});
+            _table.Stock6.Add(new Card(CardValue.Two, Color.Spade) {IsReversed = true});
+            _table.Stock7.Add(new Card(CardValue.Four, Color.Club) {IsReversed = true});
+        }
+
+        [Test]
+        public void TryMovingFromNonExistingStock()
+        {
+            Assert.Throws(typeof(ArgumentException),
+                () => _cardMover.MoveCard(_table, -1, 2, new Card(CardValue.Ace, Color.Diamond)));
+            Assert.Throws(typeof(ArgumentException),
+                () => _cardMover.MoveCard(_table, 0, 2, new Card(CardValue.Ace, Color.Diamond)));
+            Assert.Throws(typeof(ArgumentException),
+                () => _cardMover.MoveCard(_table, 8, 2, new Card(CardValue.Ace, Color.Diamond)));
+        }
+
+        [Test]
+        public void TryMovingToNonExistingStock()
+        {
+            Assert.Throws(typeof(ArgumentException),
+                () => _cardMover.MoveCard(_table, 1, -1, new Card(CardValue.Two, Color.Heart)));
+            Assert.Throws(typeof(ArgumentException),
+                () => _cardMover.MoveCard(_table, 1, 0, new Card(CardValue.Two, Color.Heart)));
+            Assert.Throws(typeof(ArgumentException),
+                () => _cardMover.MoveCard(_table, 1, 8, new Card(CardValue.Two, Color.Heart)));
+        }
+    }
+}
---
+++ Pasjans/Pasjans/CardMover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pasjans.PlayingCard;
+
+namespace Pasjans
+{
+    public class CardMover
+    {
+        public CardMover()
+        {
+
+        }
+
+        public Table MoveCard(Table table, int from, int to, Card card)
+        {
+            return table;
+        }
+    }
+}
