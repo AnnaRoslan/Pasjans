8b562202d8ca8441207f333c4f60929cec1cba44
= <=>
2020-11-15 22:25:51 +0100

    Adding CardsColumnFactory_Create_ThrowArgumentException_NullCardPackArgument_Test for Create method in CardsColumnFactory

---
+++ Pasjans/TableTests/CardsColumnFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Table;
+using Xunit;
+
+namespace TableTests
+{
+    public class CardsColumnFactoryTests
+    {
+        [Fact]
+        public void CardsColumnFactory_Create_ThrowArgumentException_NullCardPackArgument_Test()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                {
+                    var factory = new CardsColumnFactory();
+                    factory.Create(null, 1);
+                }
+            );
+        }
+    }
+}
