4a83314e19977247c68c1f9ccea18930461521d0
= <=>
2020-11-16 00:24:16 +0100

    Adding CardsColumnFactory_Create_ReturnProperCardsColumn_Test() to Create method in CardsColumnFactory.cs

--- Pasjans/TableTests/CardsColumnFactoryTests.cs
+++ Pasjans/TableTests/CardsColumnFactoryTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using CardPack;
+using FluentAssertions;
 using Table;
 using Xunit;
 
@@ -47,6 +49,33 @@ namespace TableTests
                     factory.Create(cardPack, 2);
                 }
             );
+        }
+
+        [Fact]
+        public void CardsColumnFactory_Create_ReturnProperCardsColumn_Test()
+        {
+            var cardPack = new List<Card>
+            {
+                new Card(CardColour.Club, CardValue.Ace),
+                new Card(CardColour.Diamond, CardValue.Eight),
+                new Card(CardColour.Heart, CardValue.Five),
+                new Card(CardColour.Spade, CardValue.Jack)
+            };
+
+            var expectedHiddenCards = cardPack.GetRange(0, 3);
+            var expectedVisibleCards = cardPack.GetRange(3, 1);
+
+            var hiddenCardsProperty =
+                typeof(CardsColumn).GetField("_hiddenCards", BindingFlags.NonPublic | BindingFlags.Instance);
+            var visibleCardsProperty =
+                typeof(CardsColumn).GetField("_visibleCards", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var factory = new CardsColumnFactory();
+            var cardsColumn = factory.Create(cardPack, 4);
+
+            hiddenCardsProperty.GetValue(cardsColumn).Should().BeEquivalentTo(expectedHiddenCards);
+            visibleCardsProperty.GetValue(cardsColumn).Should().BeEquivalentTo(expectedVisibleCards);
+            cardPack.Should().BeEmpty();
         }
     }
 }
