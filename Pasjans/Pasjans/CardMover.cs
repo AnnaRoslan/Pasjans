8ecd5b0002d4172de5ccc00e91af3fcc6d47b57c
Cezary Michalski <cezary.michalski2.stud@pw.edu.pl>
2020-11-15 22:36:51 +0100

    Moving from and to non existing stock tests passed.

--- Pasjans/Pasjans/CardMover.cs
+++ Pasjans/Pasjans/CardMover.cs
@@ -14,6 +14,30 @@ namespace Pasjans
 
         public Table MoveCard(Table table, int from, int to, Card card)
         {
+            var fromStock = from switch
+            {
+                1 => table.Stock1,
+                2 => table.Stock2,
+                3 => table.Stock3,
+                4 => table.Stock4,
+                5 => table.Stock5,
+                6 => table.Stock6,
+                7 => table.Stock7,
+                _ => throw new ArgumentException("Given stock does not exist.")
+            };
+
+            var toStock = to switch
+            {
+                1 => table.Stock1,
+                2 => table.Stock2,
+                3 => table.Stock3,
+                4 => table.Stock4,
+                5 => table.Stock5,
+                6 => table.Stock6,
+                7 => table.Stock7,
+                _ => throw new ArgumentException("Given stock does not exist.")
+            };
+
             return table;
         }
     }
