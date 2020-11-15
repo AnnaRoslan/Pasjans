15d2faade586544478b6fd950975d51ad43fb4d8
= <=>
2020-11-15 22:48:07 +0100

    Adding CardsColumnFactory_Create_ThrowArgumentException_NegativeColumnCapacityArgument_Test for Create method in CardsColumnFactory

--- Pasjans/TableTests/CardsColumnFactoryTests.cs
+++ Pasjans/TableTests/CardsColumnFactoryTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CardPack;
 using Table;
 using Xunit;
 
@@ -13,6 +15,23 @@ namespace TableTests
                 {
                     var factory = new CardsColumnFactory();
                     factory.Create(null, 1);
+                }
+            );
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(int.MinValue)]
+        public void CardsColumnFactory_Create_ThrowArgumentException_ZeroOrLessColumnCapacityArgument_Test(
+            int columnCapacity)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                {
+                    var cardPack = new List<Card>();
+
+                    var factory = new CardsColumnFactory();
+                    factory.Create(cardPack, columnCapacity);
                 }
             );
         }
