93ccdd178c5a3f04bee66ed63ca76b1add3e0125
Kacper Jarczak <01142567@pw.edu.pl>
2020-11-15 23:41:34 +0100

    Visualization improvement

--- Pasjans/Pasjans/GameManager.cs
+++ Pasjans/Pasjans/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
+using Pasjans.PlayingCard;
 
 namespace Pasjans
 {
@@ -8,54 +9,78 @@ namespace Pasjans
         public void StartGame()
         {
             var isWin = false;
+            var table = new Table(new Deck());
             do
             {
-                PrintBoard(null);
+                PrintTable(table);
                 var userInput = Console.ReadLine();
                 var spots = userInput.Split(" ");
+
+                if ("RD".Equals(spots?[1], StringComparison.OrdinalIgnoreCase))
+                {
+
+                }else if ("RC".Equals(spots?[1], StringComparison.OrdinalIgnoreCase))
+                {
+
+                }else if ("S1".Equals(spots?[1], StringComparison.OrdinalIgnoreCase))
+                {
+
+                }else if ("S2".Equals(spots?[1], StringComparison.OrdinalIgnoreCase))
+                {
+
+                }else if ("S3".Equals(spots?[1], StringComparison.OrdinalIgnoreCase))
+                {
+
+                }else if ("S4".Equals(spots?[1], StringComparison.OrdinalIgnoreCase))
+                {
+
+                }
 
             } while (!isWin);
         }
 
-        private void PrintBoard(List<string> board)
+        public Table MoveCard(Table table, int from, int to, Card card)
         {
-            var fd = "XX";
-            var fc = "  ";
-            var s1 = "1Ż";
-            var s2 = "1Ż";
-            var s3 = "1Ż";
-            var s4 = "1Ż";
-            var n = 5;
-
+            return table;
+        }
+        private void PrintTable(Table table)
+        {
+            var rd = "██";
+            var rc = table.ReserveStock.Count > 0 ? table.ReserveStock.Last().ToString() : "  ";
+            var s1 = table.FinalStock1.Count > 0 ? table.FinalStock1.Last().ToString() : "  ";
+            var s2 = table.FinalStock2.Count > 0 ? table.FinalStock2.Last().ToString() : "  ";
+            var s3 = table.FinalStock3.Count > 0 ? table.FinalStock3.Last().ToString() : "  ";
+            var s4 = table.FinalStock4.Count > 0 ? table.FinalStock4.Last().ToString() : "  ";
+            var N = new[]
+            {
+                table.Stock1.Count,
+                table.Stock2.Count,
+                table.Stock3.Count,
+                table.Stock4.Count,
+                table.Stock5.Count,
+                table.Stock6.Count,
+                table.Stock7.Count
+            };
 
             Console.Clear();
             Console.WriteLine("__________________Solitaire________________________");
-            Console.WriteLine($"  -FD-   -FC-          -S1-   -S2-   -S3-   -S4-  ");
-            Console.WriteLine($" | {fd} |>| {fc} |        | {s1} | | {s2} | | {s3} | | {s4} | ");
+            Console.WriteLine($"  -RD-   -RC-          -S1-   -S2-   -S3-   -S4-  ");
+            Console.WriteLine($" |{rd,3} |>|{rc,3} |        |{s1,3} | |{s2,3} | |{s3,3} | |{s4,3} | ");
             Console.WriteLine($"  ----   ----          ----   ----   ----   ----  ");
             Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
             Console.WriteLine($"  - 1-   - 2-   - 3-   - 4-   - 5-   - 6-   - 7-  ");
 
-            for (var i = 0; i < n; i++)
+            for (var i = 0; i < N.Max(); i++)
             {
-                var c1 = "| 2C |";
-                var c2 = "| XX |";
-                var c3 = "| 2C |";
-                var c4 = "| XX |";
-                var c5 = "| XX |";
-                var c6 = "| 2C |";
-                var c7 = "| XX |";
+                var c1 = i < N[0] ? $"|{table.Stock1[i].ToString(),3} |" : "      ";
+                var c2 = i < N[1] ? $"|{table.Stock2[i].ToString(),3} |" : "      ";
+                var c3 = i < N[2] ? $"|{table.Stock3[i].ToString(),3} |" : "      ";
+                var c4 = i < N[3] ? $"|{table.Stock4[i].ToString(),3} |" : "      ";
+                var c5 = i < N[4] ? $"|{table.Stock5[i].ToString(),3} |" : "      ";
+                var c6 = i < N[5] ? $"|{table.Stock6[i].ToString(),3} |" : "      ";
+                var c7 = i < N[6] ? $"|{table.Stock7[i].ToString(),3} |" : "      ";
                 Console.WriteLine($" {c1} {c2} {c3} {c4} {c5} {c6} {c7} ");
             }
-
-
-            Console.WriteLine($" | 2C | | XX | | XX | | XX | | XX | | XX | | XX | ");
-            Console.WriteLine($" |    | | 2C | | XX | | XX | | XX | | XX | | XX | ");
-            Console.WriteLine($" |    | |    | | 2C | | XX | | XX | | XX | | XX | ");
-            Console.WriteLine($" |    | |    | |    | | 2C | | XX | | XX | | XX | ");
-            Console.WriteLine($" |    | |    | |    | |    | | 2C | | XX | | XX | ");
-            Console.WriteLine($" |    | |    | |    | |    | |    | | 2C | | XX | ");
-            Console.WriteLine($" |    | |    | |    | |    | |    | |    | | 2C | ");
 
             Console.WriteLine("__________________________________________________");
             Console.WriteLine("Your move:");
--- Pasjans/Pasjans/PlayingCard/Card.cs
+++ Pasjans/Pasjans/PlayingCard/Card.cs
@@ -1,4 +1,6 @@
-﻿namespace Pasjans.PlayingCard
+﻿using System.Diagnostics;
+
+namespace Pasjans.PlayingCard
 {
     public class Card
     {
@@ -10,6 +12,27 @@
         {
             Color = color;
             CardValue = value;
+        }
+
+        public override string ToString()
+        {
+            var c = Color switch
+            {
+                Color.Club => "C",
+                Color.Diamond =>"D",
+                Color.Heart => "H",
+                Color.Spade => "S"
+            };
+
+            var value = CardValue switch
+            {
+                CardValue.Ace => "A",
+                CardValue.Jack=> "J",
+                CardValue.Queen=> "Q",
+                CardValue.King=> "K",
+                _=> ((int)CardValue).ToString()
+            };
+            return IsReversed ? value+c : "██";
         }
     }
 }
