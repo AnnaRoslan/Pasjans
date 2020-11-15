89e747a52d9e3afa8c50db6d97256fefc5e311d7
= <=>
2020-11-15 21:20:01 +0100

    Adding DefaultPack_Get_Return52UniqueCards_Test to DefaultPack method Get

---
+++ Pasjans/CardPackTests/DefaultPackTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using CardPack;
+using Xunit;
+
+namespace CardPackTests
+{
+    public class DefaultPackTests
+    {
+        [Fact]
+        public void DefaultPack_Get_Return52UniqueCards_Test()
+        {
+            var pack = DefaultPack.Get();
+            Assert.Equal(52, pack.Distinct().Count());
+        }
+    }
+}
