84e92bcfcdcbf14a29da1972ea368b28c572d371
Anna Rosłan <anna.roslan.stud@pw.edu.pl>
2020-11-15 18:49:37 +0100

    add basic class with props

---
+++ Pasjans/Pasjans/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pasjans
+{
+   public class Card
+    {
+        public Color color { get; set; }
+        public CardType CardType { get; set; }
+
+    }
+}
---
+++ Pasjans/Pasjans/Color.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pasjans
+{
+    public enum Color
+    {
+        Heart = 0, // ♥
+        Spade = 1, // ♠ 
+        Diamond = 2, // ♦ 
+        Club = 3 // ♣ 
+
+    }
+    
+        
+    
+}
