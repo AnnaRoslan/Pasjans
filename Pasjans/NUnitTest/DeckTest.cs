c894f5b5b30757a8c59f67f3023a72719fdcbbeb
Anna Rosłan <anna.roslan.stud@pw.edu.pl>
2020-11-15 19:32:22 +0100

    deck have 52 card green test

---
+++ Pasjans/NUnitTest/DeckTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Pasjans;
+
+namespace NUnitTest
+{
+    class DeckTest
+    {
+        private Deck deck;
+        [SetUp]
+        public void setUp()
+        {
+            deck = new Deck();
+        }
+        [Test]
+        public void Is_deck_have_52_cards()
+        {
+            Assert.AreEqual(deck.DeckCards.Count,52);
+        }
+    }
+}
---
+++ Pasjans/Pasjans/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pasjans
+{
+    public class Deck
+    {
+        public List<Card> DeckCards  = new List<Card>();
+
+        public Deck()
+        {
+            for (int i = 0; i < 52; i++)
+            {
+                DeckCards.Add(new Card(CardValue.Ace,Color.Club));
+            }
+        }
+        
+
+    }
+}
