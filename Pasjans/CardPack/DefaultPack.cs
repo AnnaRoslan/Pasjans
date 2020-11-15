63722edf25e5d44d47dcced15ad54f30a24f6afd
= <=>
2020-11-15 20:33:07 +0100

    Adding DefaultPack.cs

---
+++ Pasjans/CardPack/DefaultPack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardPack
+{
+    public static class DefaultPack
+    {
+        private const int ColoursNumber = 4;
+        private const int ValuesNumber = 13;
+
+        public static IReadOnlyList<Card> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
