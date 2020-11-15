a7febd5c38f0b41ab5b3f03daf9849bd8a2046b9
= <=>
2020-11-15 21:26:20 +0100

    Adding IShuffledCardPack.cs, ShuffledCardPack.cs

---
+++ Pasjans/CardPack/IShuffledCardPack.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CardPack
+{
+    public interface IShuffledCardPack
+    {
+        IReadOnlyList<Card> GetShuffledCardPack();
+    }
+}
---
+++ Pasjans/CardPack/ShuffledCardPack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CardPack
+{
+    public class ShuffledCardPack : IShuffledCardPack
+    {
+        public IReadOnlyList<Card> GetShuffledCardPack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
